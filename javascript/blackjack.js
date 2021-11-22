2384f4d4e65de9780ce1d727cc66cfe5dddd9258
Yash Khemani <ykhemani2@gmail.com>
2021-11-22 19:03:25 +0530

    added Bootstrap css

--- javascript/blackjack.js
+++ javascript/blackjack.js
@@ -2,8 +2,20 @@ let cards = [];
 let sum = 0;
 let hasBlackjack = false;
 let isAlive = false;
+let player = {
+    Name : "",
+    Chips : "",
+    playerProfile:function(){
+        this.Name = prompt("Enter Player Name");
+    }
+}
+
+player.playerProfile();
+
 
 let message="";
+let playerMessage = "";
+let playerEl = document.getElementById("playerProfile");
 let messageEl = document.getElementById("result");
 let sumEl = document.getElementById("sum");
 let cardsEl = document.getElementById("cards");
@@ -23,7 +35,9 @@ newCardBtn.disabled = true;
     if(sum > 21)
     {
         isAlive = false;
-        message = "You Lost";
+        message = "You Lost! Cilck Start for New Game";
+        player.Chips = "$0";
+
         
         setTimeout(function (){
         startBtn.disabled = false;
@@ -37,7 +51,8 @@ newCardBtn.disabled = true;
     {
         isAlive = true;
         hasBlackjack = true;
-        message = "Blackjack";
+        message = "Blackjack!! Cilck Start for New Game";
+        player.Chips = "$200";
 
         setTimeout(function() {
         startBtn.disabled = false;
@@ -51,14 +66,18 @@ newCardBtn.disabled = true;
         isAlive = true;
         message = "Draw another card";
         newCardBtn.disabled = false;
+        player.Chips = "$0";
     }
     messageEl.textContent = message;
+    playerMessage = player.Name + ": " + player.Chips;
+    playerEl.textContent = playerMessage;
     
 }
 
 function gameStart()
 {
     setTimeout(function() { startBtn.disabled = true;},200)
+    
     isAlive = true;
     hasBlackjack = false;
     let firstCard = generateCard();
